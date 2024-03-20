2fa04298b0f671ddadd5f1a76577b76e72f2982e
surya-kapila <surya@thejavascriptshop.com>
2024-03-20 11:52:18 +0530

    maints prot update

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ dotenv.config();
 
 const imports: any[] =
   process.env.NODE_ENV === 'dev' ? [ConfigModule.forRoot()] : [];
-process.env.APP_VERSION = '0.0.5';
+process.env.APP_VERSION = '0.0.6';
 @Module({
   imports: [
     ...imports,
