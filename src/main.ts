c3b7222fea741a66a28bc97ce87263f10924155c
surya-kapila <surya@thejavascriptshop.com>
2024-03-20 13:52:58 +0530

    updated port and mongoDB

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { json, urlencoded } from 'express';
+import mongoose from 'mongoose';
+import { Request, Response } from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+  const port = process.env.PORT || 3001;
+  app.enableCors();
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
+
+  const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(process.env.DATABASE_URL);
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+
+  //routes go here
+  // ...
+
+  app.use('*', (req: Request, res: Response) => {
+    res.json({ everything: 'is awesome' });
+  });
+  connectDB().then(async () => {
+    await app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  });
 }
 bootstrap();
