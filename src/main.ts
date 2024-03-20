94c7a865cefed19f69e460a8e7db18ce56143678
surya-kapila <surya@thejavascriptshop.com>
2024-03-20 16:09:51 +0530

    +1

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import mongoose from 'mongoose';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
 
 async function bootstrap() {
-  const server = express();
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3001;
-
-  server.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
 
   const connectDB = async () => {
     try {
